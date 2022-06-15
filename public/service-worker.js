b66bb59514a3f414856111aa08764d4d94ac5b46
Johan Griesel <johan@stratusolve.com>
2022-06-15 08:44:12 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,12 +25,12 @@ const PRECACHE_URLS = [
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(PRECACHE)
-        .then(cache => cache.addAll(PRECACHE_URLS))
-        .then(self.skipWaiting())
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
     );
 });
 
-self.addEventListener('message', function(event) {
+self.addEventListener('message', function (event) {
     console.log("Message: " + JSON.stringify(event.data));
     if (event.data.action === 'skipWaiting') {
         self.skipWaiting();
@@ -55,53 +55,24 @@ self.addEventListener('activate', event => {
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        if (event.request.mode === 'navigate') {
-            event.respondWith(
-                fetch(event.request)
-                .then((response) => {
-                    return response;
-                })
-                .catch(() => caches.match(OFFLINE_URL)).then(function(response) {
-                    if (response) {
-                        return response;
-                    }
-                }));
-        } else {
-            return fetch(event.request);
-        }
-    } else {
-        return fetch(event.request);
-    }
-    //TODO: Figure this out...
+    // TODO: Test if this doesn't mess up caches when a new version of the app is built for production
     // Skip cross-origin requests, like those for Google Analytics.
-    /*if (event.request.url.startsWith(self.location.origin)) {
-        if (event.request.mode === 'navigate') {
-            event.respondWith(
-                fetch(event.request)
-                .then((response) => {
-                    return response;
-                })
-                .catch(() => caches.match(OFFLINE_URL)).then(function(response) {
-                    if (response) {
-                        return response;
-                    }
-                }));
-        } else {
-            console.log("Not navigate");
-            event.respondWith(
-                caches.match(event.request)
-                .then(cachedResponse => {
-                    if (cachedResponse) {
-                        return cachedResponse;
-                    }
-                }, () => {
-                    return fetch(event.request);
-                }).catch(() => {
-                    return fetch(event.request);
-                }));
-        }
-    } else {
-        return fetch(event.request);
-    }*/
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
 });
