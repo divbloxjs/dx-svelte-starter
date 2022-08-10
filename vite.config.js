b8f4ce943ffe64c46aa82236842d7f8ce1731e71
Dani <dani.simeonov@stratusolve.com>
2022-08-10 06:29:13 +0200

    Updated viteconfig to use the created fixed paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-    plugins: [svelte({ useVitePreprocess: true })],
+    plugins: [svelte({ useVitePreprocess: true, typescript: true })],
+    resolve: {
+        alias: {
+            $src: path.resolve("./src"),
+            $base_components: path.resolve("./src/base_components"),
+            $assets: path.resolve("./src/assets"),
+            $lib: path.resolve("./src/lib"),
+            $navigation: path.resolve("./src/lib/navigation"),
+            $utilities: path.resolve("./src/lib/js/utilities"),
+            $stores: path.resolve("./src/lib/js/stores"),
+        },
+    },
 });
