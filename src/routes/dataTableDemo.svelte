4b557e1f48534bd759c721bee5e2b0897fd7ad3a
Dani <dani.simeonov@stratusolve.com>
2022-08-12 18:48:00 +0200

    removed dead code

--- src/routes/dataTableDemo.svelte
+++ src/routes/dataTableDemo.svelte
@@ -138,16 +138,6 @@
             {multiSelectActions}
             on:actionTriggered={async (params) => handleActionTriggered(params)} />
     </div>
-    <!-- <main class="mt-5 mb-16 pl-5 pr-5">
-        <div class="hero h-96 max-h-screen bg-base-200">
-            <div class="hero-content text-center">
-                <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Welcome</h1>
-                    <p class="py-6">This is a "blank" home page to get you started. Go nuts!</p>
-                    <button class="btn btn-primary" on:click={openComponentLibrary}> See Component Library </button>
-                </div>
-            </div>
-        </div>
-    </main> -->
+
     <QuickActionsMenu />
 </PageTransitionFade>
