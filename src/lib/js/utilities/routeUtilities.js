8ea8800936b8fccb05f1898d05f4f80b89da10a4
git <johan@stratusolve.com>
2022-09-06 14:32:32 +0200

    Brought in some more advancements from divblox.app

--- src/lib/js/utilities/routeUtilities.js
+++ src/lib/js/utilities/routeUtilities.js
@@ -1,7 +1,21 @@
 import { push, pop, replace } from "svelte-spa-router";
 
 export const routeUtilities = {
+    goToPage: (page = null, params = null) => {
+        if (params !== null) {
+            push("/" + page + "/" + params.join("/"));
+        } else {
+            push("/" + page);
+        }
+    },
     goBack: () => {
         pop();
+    },
+    goToLogin: (message = null) => {
+        if (message !== null) {
+            push("/login/" + message);
+        } else {
+            push("/login");
+        }
     },
 };
